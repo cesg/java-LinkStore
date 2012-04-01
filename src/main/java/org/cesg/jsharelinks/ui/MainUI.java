79c031a8d8f500be7394690049d7df6db32378fa
cesg <cesg.dev@gmail.com>
2012-04-01 18:54:51 -0400

    cambios en MainUi

--- src/main/java/org/cesg/jsharelinks/ui/MainUI.java
+++ src/main/java/org/cesg/jsharelinks/ui/MainUI.java
@@ -3,18 +3,25 @@ package org.cesg.jsharelinks.ui;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JMenuBar;
 import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
 import javax.swing.ListSelectionModel;
+import javax.swing.table.DefaultTableModel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MainUI {
+
+    private final static Logger _logger = LoggerFactory.getLogger(MainUI.class);
 
     private JFrame frame;
     private JMenuBar menuBar;
     private JMenu mnOpciones;
     private JTable table;
+    private JScrollPane scrollPane;
 
 //    /**
 //     * Launch the application.
@@ -39,7 +46,7 @@ public class MainUI {
                     MainUI window = new MainUI();
                     window.frame.setVisible(true);
                 } catch ( Exception e ) {
-                    e.printStackTrace();
+                    _logger.error("## ERROR al iniciar la UI.", e);
                 }
             }
         });
@@ -62,13 +69,18 @@ public class MainUI {
         this.frame.getContentPane().setLayout(null);
         
         this.menuBar = new JMenuBar();
-        this.menuBar.setBounds(0, 0, 434, 21);
+        this.menuBar.setBounds(0, 0, 446, 21);
         this.frame.getContentPane().add(this.menuBar);
         
         this.mnOpciones = new JMenu("Opciones");
         this.menuBar.add(this.mnOpciones);
         
+        this.scrollPane = new JScrollPane();
+        this.scrollPane.setBounds(31, 53, 380, 127);
+        this.frame.getContentPane().add(this.scrollPane);
+
         this.table = new JTable();
+        this.scrollPane.setViewportView(this.table);
         this.table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         this.table.setModel(new DefaultTableModel(
             new Object[][] {
@@ -80,17 +92,17 @@ public class MainUI {
             Class[] columnTypes = new Class[] {
                 Integer.class, String.class, String.class
             };
+            @Override
             public Class getColumnClass(int columnIndex) {
                 return columnTypes[columnIndex];
             }
             boolean[] columnEditables = new boolean[] {
                 false, false, false
             };
+            @Override
             public boolean isCellEditable(int row, int column) {
                 return columnEditables[column];
             }
         });
-        this.table.setBounds(90, 86, 235, 127);
-        this.frame.getContentPane().add(this.table);
     }
 }
