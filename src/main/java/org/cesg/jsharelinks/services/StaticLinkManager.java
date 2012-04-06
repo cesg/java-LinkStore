bfd9f3d9178a93d1f7405e5b15d4efb3c006a643
cesg <cesg.dev@gmail.com>
2012-04-06 01:55:40 -0400

    Implementación de staticLinkManager.

--- src/main/java/org/cesg/jsharelinks/services/StaticLinkManager.java
+++ src/main/java/org/cesg/jsharelinks/services/StaticLinkManager.java
@@ -1,34 +1,114 @@
 package org.cesg.jsharelinks.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.cesg.jsharelinks.ConnectioFactory;
+import org.cesg.jsharelinks.mappers.LinkMapper;
 import org.cesg.jsharelinks.models.Link;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class StaticLinkManager implements LinkManager {
 
+    private  SqlSession session;
+    private static final Logger _logger = LoggerFactory.getLogger(StaticLinkManager.class);
+    private  LinkMapper mapper;
+    
+    public StaticLinkManager() {
+        SqlSessionFactory sqlSessionFactory = ConnectioFactory.getSession();
+        if(sqlSessionFactory != null) {
+            this.session = ConnectioFactory.getSession().openSession();
+            this.mapper = this.session.getMapper(LinkMapper.class);
+        }else
+            _logger.error("# Sessión no iniciada.");
+    }
+    
+    public void closeSession() {
+        if(this.session != null)
+            this.session.close();
+    }
+    
     public Link selectLink ( Integer id) {
-        // TODO Auto-generated method stub
-        return null;
+
+        Link link = null;
+        if ( id == null )
+            return link;
+
+        try {
+            link = mapper.selectLinkById(id);
+        } catch ( Exception e ) {
+            _logger.error("Error al intentar seleccionar un Link. {}",
+                    e.getMessage());
+        } finally {
+            
+        }
+        return link;
     }
 
     public List<Link> selectAllLink () {
-        // TODO Auto-generated method stub
-        return null;
+        List<Link> allLinks = new ArrayList<Link>();
+
+        try {
+            allLinks = mapper.selectAllLink();
+        } catch ( Exception e ) {
+            _logger.error("##Error al seleccionar todas las filas.", e);
+        } finally {
+            this.session.close();
+        }
+        return allLinks;
     }
 
     public Integer insertLink ( Link link) {
-        // TODO Auto-generated method stub
-        return null;
+        Integer filasAfectadas = null;
+        if ( link == null )
+            return filasAfectadas;
+
+        try {
+            filasAfectadas = mapper.insertLink(link);
+            this.session.commit();
+        } catch ( Exception e ) {
+            _logger.error("Error al insertar el Link.", e);
+        } finally {
+            this.session.close();
+        }
+        return filasAfectadas;
     }
 
     public Integer deleteLink ( Link link) {
-        // TODO Auto-generated method stub
-        return null;
+
+        Integer filasAfectadas = null;
+        if ( link == null )
+            return filasAfectadas;
+
+        try {
+            filasAfectadas = mapper.deleteLink(link);
+            this.session.commit();
+        } catch ( Exception e ) {
+            _logger.error("Error al eliminar el link.", e);
+        } finally {
+            this.session.close();
+        }
+        return filasAfectadas;
     }
 
     public Integer deleteLinkById ( Integer id) {
-        // TODO Auto-generated method stub
-        return null;
+        
+        Integer filasAfectadas = null;
+        if ( id == null )
+            return filasAfectadas;
+
+        try {
+            filasAfectadas = mapper.deleteLinkById(id);
+            this.session.commit();
+        } catch ( Exception e ) {
+            _logger.error("Error al eliminar el link.", e);
+        } finally {
+            this.session.close();
+        }
+        return filasAfectadas;
     }
 
 }
